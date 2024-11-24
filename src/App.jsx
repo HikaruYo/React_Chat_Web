c2cbf561f8baa7de2fdc0e0374edeb81a5903a46
HikaruYo <adithaean@gmail.com>
2024-11-24 08:31:32 +0700

    Create Project and Give Background

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div
+      className="h-screen w-full bg-cover bg-center bg-no-repeat"
+      style={{
+        backgroundImage: "url('/src/assets/background.gif')",
+      }}
+    >
+      <div className="h-[100vh] flex items-center justify-center">
+        <div
+          className="w-[90vw] h-[90vh] bg-blue-400 bg-opacity-70 backdrop-blur-sm rounded-md border-2 border-gray-100 border-opacity-25"
+        >
+          app
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
