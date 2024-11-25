f634c86800b220cc571d5a917ba9443f1775a1b1
HikaruYo <adithaean@gmail.com>
2024-11-25 10:29:57 +0700

    Add input and emoji module

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import EmojiPicker from "emoji-picker-react";
 import Avatar from '../../assets/avatar2.jpg';
 
 const Chat = () => {
+  const [open, setOpen] = useState(false);
+  const [text, setText] = useState("");
+
+  const handleEmoji = e => {
+    setText(prev=>prev+e.emoji)
+    setOpen(false);
+  }
+
   return (
-    <div className="flex-[2] border-x-[1px] border-gray-400 h-full">
+    <div className="flex-[2] flex flex-col border-x-[1px] border-gray-400 h-full">
       {/* Top */}
       <div className="flex p-4 items-center justify-between border-b-[1px] border-gray-400">
         {/* User Information */}
@@ -74,13 +83,102 @@ const Chat = () => {
       </div>
 
       {/* Center */}
-      <div>
+      <div className="flex-1 p-4">
 
       </div>
 
       {/* Bottom */}
-      <div>
+      <div className="flex items-center justify-between gap-3 p-4 mt-auto border-t-[1px] border-gray-400">
+        {/* Icon */}
+        <div className="flex gap-2">
+          {/* Image Icon */}
+          <svg
+            className="w-5 h-5 cursor-pointer text-gray-800 dark:text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fillRule="evenodd"
+              d="M13 10a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2H14a1 1 0 0 1-1-1Z"
+              clipRule="evenodd"
+            />
+            <path
+              fillRule="evenodd"
+              d="M2 6a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v12c0 .556-.227 1.06-.593 1.422A.999.999 0 0 1 20.5 20H4a2.002 2.002 0 0 1-2-2V6Zm6.892 12 3.833-5.356-3.99-4.322a1 1 0 0 0-1.549.097L4 12.879V6h16v9.95l-3.257-3.619a1 1 0 0 0-1.557.088L11.2 18H8.892Z"
+              clipRule="evenodd"
+            />
+          </svg>
+          {/* Camera Icon */}
+          <svg
+            className="w-5 h-5 cursor-pointer text-gray-800 dark:text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fillRule="evenodd"
+              d="M7.5 4.586A2 2 0 0 1 8.914 4h6.172a2 2 0 0 1 1.414.586L17.914 6H19a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h1.086L7.5 4.586ZM10 12a2 2 0 1 1 4 0 2 2 0 0 1-4 0Zm2-4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Z"
+              clipRule="evenodd"
+            />
+          </svg>
+          {/* Microphone Icon */}
+          <svg
+            className="w-5 h-5 cursor-pointer text-gray-800 dark:text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fillRule="evenodd"
+              d="M5 8a1 1 0 0 1 1 1v3a4.006 4.006 0 0 0 4 4h4a4.006 4.006 0 0 0 4-4V9a1 1 0 1 1 2 0v3.001A6.006 6.006 0 0 1 14.001 18H13v2h2a1 1 0 1 1 0 2H9a1 1 0 1 1 0-2h2v-2H9.999A6.006 6.006 0 0 1 4 12.001V9a1 1 0 0 1 1-1Z"
+              clipRule="evenodd"
+            />
+            <path
+              d="M7 6a4 4 0 0 1 4-4h2a4 4 0 0 1 4 4v5a4 4 0 0 1-4 4h-2a4 4 0 0 1-4-4V6Z"
+            />
+          </svg>
+        </div>
 
+        <input
+          type="text"
+          placeholder="Type a message"
+          className="flex-1 p-2 text-sm rounded-md border-none outline-none text-white bg-blue-700 bg-opacity-50"
+          onChange={ e=>setText(e.target.value) }
+          value={ text }
+        />
+
+        {/* Emoji */}
+        <div className="relative">
+          <svg
+            className="w-5 h-5 text-gray-800 dark:text-white cursor-pointer"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+            onClick={ ()=>setOpen(prev=>!prev) }
+          >
+            <path
+              d="M21.972 11.517a.527.527 0 0 0-1.034-.105 1.377 1.377 0 0 1-1.324 1.01 1.467 1.467 0 0 1-1.4-1.009.526.526 0 0 0-1.015 0 1.467 1.467 0 0 1-2.737.143l-.049-.204.021-.146V9.369h2.304a2.632 2.632 0 0 0 2.631-2.632 2.678 2.678 0 0 0-2.654-2.632l-.526.022-.13-.369A2.632 2.632 0 0 0 13.579 2c-.461 0-.915.124-1.313.358L12 2.513l-.266-.155A2.603 2.603 0 0 0 10.422 2a2.632 2.632 0 0 0-2.483 1.759l-.13.37-.518-.024a2.681 2.681 0 0 0-2.66 2.632A2.632 2.632 0 0 0 7.264 9.37H9.61v1.887l-.007.09-.028.08a1.328 1.328 0 0 1-1.301.996 1.632 1.632 0 0 1-1.502-1.024.526.526 0 0 0-1.01.013 1.474 1.474 0 0 1-1.404 1.01 1.381 1.381 0 0 1-1.325-1.01.547.547 0 0 0-.569-.382h-.008a.526.526 0 0 0-.456.526v.446a10.012 10.012 0 0 0 10 10 9.904 9.904 0 0 0 7.067-2.94A10.019 10.019 0 0 0 22 11.966l-.028-.449ZM8.316 15.685a1.053 1.053 0 1 1 2.105 0 1.053 1.053 0 0 1-2.105 0Zm1.58 3.684a2.105 2.105 0 0 1 4.21 0h-4.21Zm4.736-2.631a1.052 1.052 0 1 1 0-2.105 1.052 1.052 0 0 1 0 2.105Z"
+            />
+          </svg>
+          {/* Picker */}
+          <div className="absolute left-0 bottom-8">
+            <EmojiPicker open={ open } onEmojiClick={ handleEmoji } />
+          </div>
+        </div>
+        <button className="p-1 px-1.5 cursor-pointer border-none rounded-md text-white bg-blue-600">Send</button>
       </div>
     </div>
   )
